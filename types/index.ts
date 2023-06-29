bc9ae74a743a0a9ff8370c561e54cb904c17f0b3
JulesNIYOMWUNGERI <julesniyomwungeri144@gmail.com>
2023-06-29 20:31:35 +0200

    landing page of the app

---
+++ types/index.ts
@@ -0,0 +1,65 @@
+import { MouseEventHandler } from "react";
+import { Interface } from "readline";
+
+export interface CustomButtonProps {
+    title: string;
+    containerStyles?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+    btnType?: "button" | "submit";
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
+}
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomerFilterProps {
+    title: string;
+    options: OptionProps[];
+
+}
+
+export interface SearchManufacturerProps {
+    manufacturer: string;
+    setManuFacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+}
+
+export interface FilterProps {
+    manufacturer: string,
+    year: number,
+    fuel: string,
+    limit: number,
+    model: string,
+}
+
+export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean;
+}
+
+export interface SearchBarProps {
+    setManufacturer: string;
+    setModel: string;
+}
+
+export interface HomeProps {
+    searchParams: FilterProps;
+  }
